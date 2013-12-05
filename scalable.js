6b282fda5a27d0501e2288d05cf3d150f2609657
Chris Dolphin <chris.dolphin@escapemg.com>
2013-12-05 15:02:54 -0500

    Adds the 'scalable' class to divs in DOM.

--- scalable.js
+++ scalable.js
@@ -1,7 +1,12 @@
-var globalObj = this;
+var globalObj = this,
+	divs = document.getElementsByTagName('div');
 for(var key in globalObj) {
     var obj = globalObj[key];
     if (obj instanceof Object) {
         obj.scalable = true;
     }
+}
+
+for(var i = 0; i < divs.length; i++) {
+	divs[i].className += ' scalable';
 }
