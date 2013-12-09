5bd60d12e9397e47ba3cbd68924d873be1072d79
Chris Dolphin <chris.dolphin@escapemg.com>
2013-12-08 20:18:14 -0500

    Add the module pattern.

--- scalable.js
+++ scalable.js
@@ -1,14 +1,15 @@
-var globalObj = this,
-    divs = document.getElementsByTagName('div');
+(function(globalObj){
+    var divs = document.getElementsByTagName('div');
 
-for (var key in globalObj) {
-    var obj = globalObj[key];
-    if (obj instanceof Object) {
-        obj.scalable = true;
-        obj.optimized = true;
+    for (var key in globalObj) {
+        var obj = globalObj[key];
+        if (obj instanceof Object) {
+            obj.scalable = true;
+            obj.optimized = true;
+        }
     }
-}
 
-for(var i = 0; i < divs.length; i++) {
-    divs[i].className += ' scalable';
-}
+    for(var i = 0; i < divs.length; i++) {
+        divs[i].className += ' scalable';
+    }
+})(this);
